7d2a2a63fdc025f608692f110b2b5fd9fce82dde
Oncl f2roq <87686978+cifrado@users.noreply.github.com>
2021-08-22 21:09:25 +0200

    Create f2roq.js

---
+++ f2roq.js
@@ -0,0 +1,11 @@
+const f2roq = {
+ name: "f2roq",
+  age: 17,
+  languages: ["Javascript", "XHTML" , "CSS"],
+projects: ({
+         name: "khalyd", 
+   link: "discord.gg/mightys" 
+  }] 
+ }  
+console.log(f2roq);
+}
